529b44c305a5b2941dca499e492987dc76c07dc8
MatiPy <matias.pyyhtia@student.saimia.fi>
2019-03-06 20:46:19 +0200

    Added Loop-Task-1

---
+++ Loop-Tasks-1/LoopTask1/LoopTask1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace loopTask3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma laskeee annetun luvun kertoman");
+            
+            int number = 0;
+            int i = 1;
+            int fact = 1;
+            do
+            {
+
+
+                Console.Write("syötä luku:");
+                string userInput = Console.ReadLine();
+                number = int.Parse(userInput);
+
+
+                if (number <= 0)
+                {
+                    Console.WriteLine("virheellinen syöte!");
+                }
+            } while (number <= 0);
+                                                                                         //calculate factorial 1 * 2 * 3 * ... * n
+                while (i <= number)
+                {
+                fact = fact * i;    //fact*=i
+                i = i + 1;         //i++;
+
+                }
+
+                Console.WriteLine($"Luvun {number} ! = {fact}");
+
+                                                                                                /*
+                                                                                                 * for (i = 1; i <=; i++)
+                                                                                                 * {
+                                                                                                 *      fact * = i;
+                                                                                                 * }
+                                                                                                 * */
+     
+      
+
+        }
+    }
+}
---
+++ Loop-Tasks-1/LoopTask3/LoopTask3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace loopTask3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            int i = 1;
+            int evenSum = 0;
+            int oddSum = 0;
+           
+            Console.WriteLine("Ohjelma laskee parittomien ja parillisten lukujen summan!");
+            Console.Write("syötä lukusi:");
+            number = int.Parse(Console.ReadLine());
+            
+            while(i <= number)
+            {
+                if (i % 2 == 0)
+                {
+                    evenSum = evenSum + i; 
+                    
+                }
+                
+                else
+                {
+                    oddSum = oddSum + i; 
+                    
+
+                }
+                i++;
+            }
+            Console.WriteLine($"parillisten summa on {evenSum}");
+            Console.WriteLine($"parittomien summa on {oddSum}");
+
+
+        }
+    }
+}
