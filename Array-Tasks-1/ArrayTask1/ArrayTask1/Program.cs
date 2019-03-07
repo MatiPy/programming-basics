6bf195a8da97abb1a69150c27d5b8c37c7d7ea55
MatiPy <matias.pyyhtia@student.saimia.fi>
2019-03-07 10:23:55 +0200

    Added Array-Tasks-1

---
+++ Array-Tasks-1/ArrayTask1/ArrayTask1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ArrayTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ohjelma alustaa kokonaisluku taulukon(10 alkiota) johon arvotaan lukuja 0....100");
+            int[] numbers = new int[10];
+            Random rnd = new Random();
+          
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rnd.Next(100);
+            }
+         
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+
+
+            Console.WriteLine("ohjelma päättyy");
+
+        }
+
+    }
+}
---
+++ Array-Tasks-1/ArrayTask2/ArrayTask2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Globalization;
+namespace ArrayTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            //CultureInfo culture = new CultureInfo("sv-SV");
+            CultureInfo.CurrentCulture = new CultureInfo("fi-FI");
+            Console.WriteLine(DateTime.Now);
+
+            Console.WriteLine("ohjelma alustaa kokonaisluku taulukon(10 alkiota) johon arvotaan lukuja 0....50");
+            int[] iT = new int[100];
+            Random rnd = new Random();
+           
+          
+            for (int i = 0; i < iT.Length; i++)
+            {
+                iT[i] = rnd.Next(50);
+            }
+         
+
+            for (int i = 0; i < iT.Length; i++)
+            {
+
+                 int Sum(params int[] iT)
+                {
+                    int result = 0;
+
+                    for (int i = 0; i < iT.Length; i++)
+                    {
+                        result += iT[i];
+                    }
+
+                    return result;
+                }
+
+                
+                    decimal Average(params int[] iT)
+                { 
+                    int sum = Sum(iT);
+                    decimal keskiarvo = Average(iT);
+                    decimal result = (decimal)sum / iT.Length;
+                    return result;
+                }
+
+            }
+
+
+            Console.WriteLine("ohjelma päättyy");
+        }
+    }
+}
