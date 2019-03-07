01c10eecf1ac1f6aa3d788ff6d1462663252c0e1
MatiPy <matias.pyyhtia@student.saimia.fi>
2019-03-07 11:42:26 +0200

    Added VokaaliLaskuri

---
+++ Vokaalilaskuri/VokaaliLaskuri/VokaaliLaskuri/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace VokaaliLaskuri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tämä on vokaalilaskuri-ohjelma!\n");
+            Console.WriteLine("Anna ohjelmalle viesti, ohjelma laskee vokaaleiden määrän\n");
+
+            string message = UserInput();
+            Console.WriteLine($"Viestissäsi: {message} oli yhteensä {VCalc(message)} vokaalia!\n");
+        }
+        static string UserInput()
+        {
+            Console.Write("Syötä ohjelmalle viestisi:\n"); string userInput = Console.ReadLine(); return userInput;
+        }
+        static int VCalc(string usermessage)
+        {
+            usermessage = usermessage.ToLower(); int count = 0;
+            for (int i = 0; i < usermessage.Length; i++)
+            {
+                if (usermessage[i] == 'a' ||
+                    usermessage[i] == 'e' ||
+                    usermessage[i] == 'i' ||
+                    usermessage[i] == 'o' ||
+                    usermessage[i] == 'u' ||
+                    usermessage[i] == 'y' ||
+                    usermessage[i] == 'ä' ||
+                    usermessage[i] == 'ö')
+                {
+                    count++;
+                }
+            }
+            return count;
+
+        }
+    }
+}
