a57b37555a9ff446508d6a8f0413e27cb4999f7c
MatiPy <matias.pyyhtia@student.saimia.fi>
2019-03-07 11:35:22 +0200

    Added Itsearviointi

---
+++ Itsearviointi/itsearviointi1_1/itsearviointi1_1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace itsearviointi1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma toistaa käyttäjän kirjoittaman lauseen viisinkertaisesti");
+            Console.WriteLine("Kirjoita ohjelmalle lause!");
+
+            int n;
+            int userInput;
+
+            
+
+
+            while (true)
+            {
+                Console.WriteLine("lauseesi:");
+                n = int.Parse(Console.ReadLine());
+                // string userInput = Console.ReadLine();
+                // number = int.TryParse(userInput);
+
+                if (n >= 5)
+                {
+
+                    Console.WriteLine(Console.ReadKey());
+
+
+                }
+                else 
+                {
+                    //Console.WriteLine(Console.ReadKey());
+                    break;
+                }
+                {
+                  
+                    Console.WriteLine(n);
+                }
+            
+
+
+            }
+        }
+    }
+}
