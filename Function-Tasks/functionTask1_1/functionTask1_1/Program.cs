23478e5f5bb8f25a66a750a49fc93ca9226cd2be
MatiPy <matias.pyyhtia@student.saimia.fi>
2019-03-07 09:35:18 +0200

    Added Function-Tasks

---
+++ Function-Tasks/functionTask1_1/functionTask1_1/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+
+namespace FunctionTask1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Syötä ohjelmalle sana tai lause ohjelma tulostaa lauseesi.");
+
+
+            System.out.print("Anna viestisi: ");
+            String viesti = input.nextLine();
+            System.out.print("Syötä toistokerrat: ");
+            int kerrat = Integer.parseInt(input.nextLine());
+
+            for (int i = 0; i < kerrat; i++)
+            {
+                System.out.println(viesti);
+            }
+
+        }
+    }
+
+}
+
+
+
+
+
---
+++ Function-Tasks/functionTask1_4/functionTask1_4/functionTask1_4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace functionTask1_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("syötä ohjelmalle kymmenen lukua!");
+            largesNumber();
+        }
+
+        static int largesNumber(); (out int indx)
+        {
+            int indx = 0;
+            int largesNumber = 0;
+            for (int i = 0;  i< 10;  i++)
+        }
+        {
+
+                console.Write($"{i+1}.");
+                int userNumber = int.Parse(Console.ReadLine());
+
+
+            if (userNumber< 0)
+        {
+            
+                console.WriteLine("väärä syöte, syötä positiivinen luku");
+                i--;
+            
+            if (userNumber > LargesNumber)
+                largesNumber = userNumber;
+
+		}	
+            return largesNumber;
+
+
+        
+    }
+}
